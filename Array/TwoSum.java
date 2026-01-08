7f90133f2094428ff7ff8e3610ae40303f8ff0a0
Hoang Phuc <phucqaqs@gmail.com>
2026-01-08 15:30:24 +0700

    Update TwoSum HashMap

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -22,7 +22,7 @@ public class TwoSum {
     }
 
     /*
-    Brute force (O^n2)
+    Brute force (On^2)
     public int[] twoSum(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++){
             for(j = i + 1; j < nums.length; j++){
